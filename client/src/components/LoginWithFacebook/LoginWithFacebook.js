e40ab1ea0c227d83d5c2f9cb370f404e3c8ea41b
Mohamed-Zkaria <mohamed.zakaria_10_7@yahoo.com>
2020-06-23 08:30:09 +0200

    Bug fix

--- client/src/components/LoginWithFacebook/LoginWithFacebook.js
+++ client/src/components/LoginWithFacebook/LoginWithFacebook.js
@@ -1,31 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState, useHistory } from 'react';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFacebook } from '@fortawesome/free-brands-svg-icons';
 
 import FacebookLogin from 'react-facebook-login/dist/facebook-login-render-props';
-
+import axios from "../../axios";
 function LoginWithFacebook(props){
     const responseFacebook = (response) => {
-        console.log("response");
-        props.methods.email(response.email);
-        props.methods.faceID(response.id);
-        props.methods.logMethod("facebook");
-        props.methods.name(response.name);
-        props.methods.submit();
-    }
-
-    const componentClicked = (e) => {
-        console.log("com clicked.")
+        let name, email, loginMethod, facebookID;
+        if(!response.name || !response.email || !response.id ){
+            props.methods.setError("Not all data fetched, please try again.");
+        } else {
+            axios
+            .post('teacher/login', { email: response.email, loginMethod: "facebook", facebookID: response.id, name: response.name })
+            .then((result) => {
+              localStorage.setItem('teacherToken', result.data.token);
+              props.history.push('/teacher');
+            })
+            .catch((err) => {
+              console.log(err);
+              props.methods.setError('invalid credentials');
+            });
+        }
     }
 
     return(
 
         <FacebookLogin
             appId="614250152549427"
-            autoLoad
+            autoLoad={false}
             callback={responseFacebook}
-            onClick={componentClicked}
             fields="name,email"
             render={renderProps => (
                 <button type="btton" onClick={renderProps.onClick} className="btn btn-block bg-info text-white">
