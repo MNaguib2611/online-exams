62873bf88219adf16f4d99f91e459a9722ab053f
Islam <iabdelhamid3@gmail.com>
2020-06-29 08:18:20 +0200

    fix small issue

--- client/src/pages/Student/Exam/Exam.js
+++ client/src/pages/Student/Exam/Exam.js
@@ -12,8 +12,6 @@ import Loading from '../../../components/Loading/Loading';
 import CalcSc from '../../../components/CalcSc/CalcSc';
 import Notes from '../../../components/Notes/Notes';
 
-var timeIsUp = false;
-
 export const Exam = (props) => {
   const { addToast } = useToasts();
 
@@ -82,83 +80,59 @@ export const Exam = (props) => {
       answersToSubmit.push({ _id: key, answer: answers[key] });
     });
 
-    if (timeIsUp) {
-      axios
-        .post(
-          '/students/exams/' + id + '/answers/',
-          { answers: answersToSubmit },
-          {
-            headers: {
-              'x-access-token': localStorage.getItem('studentToken'),
-            },
-          }
-        )
-        .then((res) => {
-          history.push('/student/exams/' + id + '/score');
-
-          addToast('time is up, exam was submitted automatically', {
-            appearance: 'error',
-            autoDismiss: true,
-          });
-        })
-        .catch((err) => {
-          console.log(err.response);
-        });
-    } else {
-      confirmAlert({
-        customUI: ({ onClose }) => {
-          return (
-            <div className='exam-card'>
-              <div className='heading'>
-                <h5>{msg}</h5>
-              </div>
-              <div className='body'>
-                {shortMsg && (
-                  <p className='text-danger'>
-                    {shortMsg}
-                    <i className='fas fa-exclamation-triangle'></i>
-                  </p>
-                )}
-                <button
-                  className='btn btn-blue mr-2'
-                  onClick={() => {
-                    axios
-                      .post(
-                        '/students/exams/' + id + '/answers/',
-                        { answers: answersToSubmit },
-                        {
-                          headers: {
-                            'x-access-token': localStorage.getItem(
-                              'studentToken'
-                            ),
-                          },
-                        }
-                      )
-                      .then((res) => {
-                        onClose();
-                        history.push('/student/exams/' + id + '/score');
-                      })
-                      .catch((err) => {
-                        console.log(err.response);
-                        // onClose();
-                        // localStorage.removeItem('answers' + exam._id);
-                        // localStorage.removeItem('studentToken');
-                        // localStorage.removeItem('answers' + exam._id);
-                        // history.push('/');
-                      });
-                  }}
-                >
-                  Yes!
-                </button>
-                <button className='btn btn-danger' onClick={onClose}>
-                  No
-                </button>
-              </div>
+    confirmAlert({
+      customUI: ({ onClose }) => {
+        return (
+          <div className='exam-card'>
+            <div className='heading'>
+              <h5>{msg}</h5>
             </div>
-          );
-        },
-      });
-    }
+            <div className='body'>
+              {shortMsg && (
+                <p className='text-danger'>
+                  {shortMsg}
+                  <i className='fas fa-exclamation-triangle'></i>
+                </p>
+              )}
+              <button
+                className='btn btn-blue mr-2'
+                onClick={() => {
+                  axios
+                    .post(
+                      '/students/exams/' + id + '/answers/',
+                      { answers: answersToSubmit },
+                      {
+                        headers: {
+                          'x-access-token': localStorage.getItem(
+                            'studentToken'
+                          ),
+                        },
+                      }
+                    )
+                    .then((res) => {
+                      onClose();
+                      history.push('/student/exams/' + id + '/score');
+                    })
+                    .catch((err) => {
+                      console.log(err.response);
+                      // onClose();
+                      // localStorage.removeItem('answers' + exam._id);
+                      // localStorage.removeItem('studentToken');
+                      // localStorage.removeItem('answers' + exam._id);
+                      // history.push('/');
+                    });
+                }}
+              >
+                Yes!
+              </button>
+              <button className='btn btn-danger' onClick={onClose}>
+                No
+              </button>
+            </div>
+          </div>
+        );
+      },
+    });
   };
 
   const handleNewAnswer = (id, answer) => {
@@ -209,8 +183,43 @@ export const Exam = (props) => {
                           )
                         ).toString()}
                         callback={() => {
-                          timeIsUp = true;
-                          if (canSubmit && timeIsUp) sendExam();
+                          if (canSubmit) {
+                            const answersToSubmit = [];
+                            Object.keys(answers).forEach((key) => {
+                              if (!answers[key])
+                                answersToSubmit.push({
+                                  _id: key,
+                                  answer: answers[key],
+                                });
+                            });
+
+                            axios
+                              .post(
+                                '/students/exams/' + id + '/answers/',
+                                { answers: answersToSubmit },
+                                {
+                                  headers: {
+                                    'x-access-token': localStorage.getItem(
+                                      'studentToken'
+                                    ),
+                                  },
+                                }
+                              )
+                              .then((res) => {
+                                history.push('/student/exams/' + id + '/score');
+
+                                addToast(
+                                  'time is up, exam was submitted automatically',
+                                  {
+                                    appearance: 'error',
+                                    autoDismiss: true,
+                                  }
+                                );
+                              })
+                              .catch((err) => {
+                                console.log(err.response);
+                              });
+                          }
                         }}
                       />
                     )) || <span> Hours : Minutes : Seconds</span>}
